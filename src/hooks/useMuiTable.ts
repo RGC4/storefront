5c898a090dbac674ee42c988118c4efbfea3a2bc
RGC4 <bob@rgc4.com>
2026-03-16 06:07:09 -0500

    Fix useMuiTable and store.config

--- src/hooks/useMuiTable.ts
+++ src/hooks/useMuiTable.ts
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-// ================================================================
 type Order = "asc" | "desc";
-  if (b[orderBy] < a[orderBy]) return -1;
-  if (b[orderBy] > a[orderBy]) return 1;
+
+function descendingComparator<T>(a: T, b: T, orderBy: string) {
+  if (b[orderBy as keyof T] < a[orderBy as keyof T]) return -1;
+  if (b[orderBy as keyof T] > a[orderBy as keyof T]) return 1;
   return 0;
 }
 
@@ -15,20 +16,21 @@ export function getComparator<T>(order: Order, orderBy: string) {
 
 export function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
   const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-
   stabilizedThis.sort((a, b) => {
     const order = comparator(a[0], b[0]);
     if (order !== 0) return order;
     return a[1] - b[1];
   });
-
   return stabilizedThis.map((el) => el[0]);
 }
 
+interface Props<T> {
   listData: T[];
   defaultSort?: string;
   defaultOrder?: "asc" | "desc";
 }
+
+export default function useMuiTable<T>({
   listData = [],
   defaultSort = "name",
   defaultOrder = "asc"
@@ -39,14 +41,12 @@ export function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
   const [selected, setSelected] = useState<string[]>([]);
   const [order, setOrder] = useState<Order>(defaultOrder);
 
-  // HANDLE DATA SORTING - (ASCENDING OR DESCENDING)
   const handleRequestSort = (property: string) => {
     const isAsc = orderBy === property && order === "asc";
     setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
-  // HANDLE SELECT ALL ROWS
   const handleSelectAllClick = (checked: boolean, defaultSelect: string) => {
     if (checked) {
       const newSelected = listData.map((item: any) => item[defaultSelect]);
@@ -56,7 +56,6 @@ export function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
     setSelected([]);
   };
 
-  // HANDLE SPECIFIC ROW OR DATA
   const handleRowClick = (name: string) => {
     const selectedIndex = selected.indexOf(name);
     let newSelected: string[] = [];
@@ -75,7 +74,6 @@ export function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
     setSelected(newSelected);
   };
 
-  // HANDLE CHANGE PAGE
   const handleChangePage = (_: unknown, newPage: number) => setPage(newPage - 1);
 
   const filteredList = stableSort(listData, getComparator(order, orderBy)).slice(
