439cb50c0d44ffa2d042cd6881cb13701536d75c
RGC4 <bob@rgc4.com>
2026-03-16 07:46:11 -0500

    Fix fashion-2 layout

--- src/app/fashion-2/layout.tsx
+++ src/app/fashion-2/layout.tsx
@@ -1,5 +1,3 @@
-import { headers } from "next/headers";
-import { getStoreConfig } from "@/lib/store-config";
 import { getLayoutData } from "utils/__api__/layout";
 import ShopLayout1 from "components/layouts/shop-layout-1/shop-layout-1";
 
@@ -8,18 +6,10 @@ export default async function Fashion2Layout({
 }: {
   children: React.ReactNode;
 }) {
-  // Get store ID injected by middleware
-  const headersList = await headers();
-  const storeId = headersList.get("x-store-id") || "s1";
-
-  // Fetch store config from Vercel Blob
-  const storeConfig = await getStoreConfig(storeId);
-
-  // Fetch layout data (nav, footer, etc.)
   const layoutData = await getLayoutData();
 
   return (
-    <ShopLayout1 data={layoutData} storeConfig={storeConfig}>
+    <ShopLayout1 data={layoutData}>
       {children}
     </ShopLayout1>
   );
