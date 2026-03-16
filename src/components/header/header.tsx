2956609658449aadf809c3b4855dcbacecbee186
RGC4 <bob@rgc4.com>
2026-03-16 04:48:16 -0500

    Fix header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, PropsWithChildren, ReactNode } from "react";
+import type { ComponentProps, PropsWithChildren } from "react";
 import Link from "next/link";
 import Box from "@mui/material/Box";
 // CUSTOM COMPONENT
@@ -7,7 +7,37 @@ import { HeaderCategoryDropdown } from "./header-category-dropdown";
 // STYLED COMPONENTS
 import { HeaderWrapper, StyledContainer } from "./styles";
 
-// =======================================================        sx={{ objectFit: "contain" }}
+// ==============================================================
+interface HeaderProps extends ComponentProps<typeof HeaderWrapper> {}
+// ==============================================================
+
+export function Header({ children, ...props }: HeaderProps) {
+  return (
+    <HeaderWrapper {...props}>
+      <StyledContainer>
+        <div>{children}</div>
+      </StyledContainer>
+    </HeaderWrapper>
+  );
+}
+
+// ==============================================================
+interface HeaderLogoProps {
+  logoUrl: string;
+  width?: number;
+  height?: number;
+}
+// ==============================================================
+
+Header.Logo = function ({ logoUrl, width = 120, height = 44 }: HeaderLogoProps) {
+  return (
+    <Link href="/">
+      <LazyImage
+        src={logoUrl}
+        alt="Store Logo"
+        width={width}
+        height={height}
+        sx={{ objectFit: "contain" }}
       />
     </Link>
   );
