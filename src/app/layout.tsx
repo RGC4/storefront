f15ec014bfa4ee382622ecfaac96280649d7e85d
RGC4 <bob@rgc4.com>
2026-03-16 07:48:48 -0500

    Add ShopLayout1 to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,15 @@ import CartProvider from "contexts/CartContext";
 import SettingsProvider from "contexts/SettingContext";
 import RTL from "components/rtl";
 import ProgressBar from "components/progress";
+import { getLayoutData } from "utils/__api__/layout";
+import ShopLayout1 from "components/layouts/shop-layout-1/shop-layout-1";
 import "overlayscrollbars/overlayscrollbars.css";
 import "i18n";
 
 export const geist = Geist({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({ children }: { children: ReactNode }) {
+  const layoutData = await getLayoutData();
   return (
     <html lang="en">
       <body className={geist.className}>
@@ -20,7 +23,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <ThemeProvider>
               <RTL>
                 <ProgressBar />
-                {children}
+                <ShopLayout1 data={layoutData}>
+                  {children}
+                </ShopLayout1>
               </RTL>
             </ThemeProvider>
           </CartProvider>
