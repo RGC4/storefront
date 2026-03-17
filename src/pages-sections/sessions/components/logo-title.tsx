e5f21aeb90082015c8320f38c8d0901fe0b4ff92
RGC4 <bob@rgc4.com>
2026-03-16 21:50:02 -0500

    add mobile hero images

--- src/pages-sections/sessions/components/logo-title.tsx
+++ src/pages-sections/sessions/components/logo-title.tsx
@@ -1,18 +1,20 @@
-﻿import Image from "next/image";
+import Image from "next/image";
 import Typography from "@mui/material/Typography";
-// CUSTOM COMPONENTS
 import FlexRowCenter from "components/flex-box/flex-row-center";
-// IMPORT IMAGES
-import logo from "../../../../public/assets/images/logo2.svg";
 
 export default function LogoWithTitle() {
   return (
     <FlexRowCenter flexDirection="column" gap={2} mb={4}>
-      <Image width={90} src={logo} alt="Prestige Apparel Group" />
+      <Image
+        width={160}
+        height={54}
+        src="/assets/stores/s1/logo/logo-header.png"
+        alt="Prestige Apparel Group"
+        style={{ objectFit: "contain" }}
+      />
       <Typography fontWeight={600} variant="h5">
         Welcome To Prestige Apparel Group
       </Typography>
     </FlexRowCenter>
   );
 }
-
