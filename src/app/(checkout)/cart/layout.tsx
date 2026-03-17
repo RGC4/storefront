9b387190b8079025c401fa8819aa8e174301fbde
RGC4 <bob@rgc4.com>
2026-03-16 21:50:02 -0500

    add mobile hero images

--- src/app/(checkout)/cart/layout.tsx
+++ src/app/(checkout)/cart/layout.tsx
@@ -4,15 +4,7 @@ import Steppers from "../steppers";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        mt: 2,
-        mb: 4,
-        px: { xs: 2, sm: 3, md: 4 },
-        overflowX: "hidden",
-      }}
-    >
+    <Container maxWidth="lg" sx={{ mt: 2, mb: 4, px: { xs: 2, sm: 3, md: 4 }, overflowX: "hidden" }}>
       <Steppers />
       {children}
     </Container>
